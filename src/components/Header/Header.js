2d825f800091f38be78795136d561a5fc39aac72
Nekhai <NekhaiYurii@gmail.com>
2022-01-24 11:37:24 +0200

    feat: add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.scss";
 
 export const Header = () => {
-  return <div>header</div>;
+  const [search, setSearch] = useState("");
+  const navigation = useNavigate();
+
+  const hanleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const formSubmit = () =>
+    navigation({
+      pathname: "/cocktail",
+      search: `?s=${search}`,
+    });
+
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__title">
+          <Link to="/">Cocktail Bar</Link>
+        </div>
+        <div className="header__content">
+          <div className="header__search">
+            <form onSubmit={formSubmit}>
+              <input
+                type="search"
+                placeholder="Search"
+                value={search}
+                onChange={hanleChange}
+              />
+            </form>
+          </div>
+          <div className="header__basket">
+            <div className="header__amount"></div>
+            <button className="header__btn">
+              <svg
+                enableBackground="new 0 0 48 48"
+                height="42px"
+                id="Layer_4"
+                version="1.1"
+                viewBox="0 0 48 48"
+                width="42px"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g>
+                  <polygon
+                    fill="#ffffff"
+                    points="21.552,26.021 21.573,26.016 21.573,26.021 41.459,26.021 41.459,26.015 41.481,26.021    45.771,10.016 17.264,10.016  "
+                  />
+                  <polygon
+                    fill="#ffffff"
+                    points="18.646,30 11.21,2.707 0,2.707 0,6.75 8.166,6.75 15.59,34 15.614,33.993 15.614,34 48,34 48,30     "
+                  />
+                  <circle cx="41.771" cy="40" fill="#ffffff" r="4" />
+                  <circle cx="19.614" cy="40" fill="#ffffff" r="4" />
+                </g>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
