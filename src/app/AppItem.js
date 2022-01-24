d12a334e53933c46948defd96e82281a39a9572e
Nekhai <NekhaiYurii@gmail.com>
2022-01-24 14:56:27 +0200

    feat: add cocktail card

--- src/app/AppItem.js
+++ src/app/AppItem.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./AppItem.scss";
 
-export const AppItem = () => {
-  return <div className="app-item"></div>;
+export const AppItem = ({ className, img, title, btn }) => {
+  return (
+    <div className={`app-item ${className}`}>
+      {img}
+      <div className="app-item__buy">
+        <h3 className="app-item__title">{title}</h3>
+        {btn}
+      </div>
+    </div>
+  );
 };
