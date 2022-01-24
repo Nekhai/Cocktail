a6613e8fcc6c8858474298139c759e72a0e4393e
Nekhai <NekhaiYurii@gmail.com>
2022-01-24 11:35:28 +0200

    feat: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./Footer.scss";
+import { alphabet } from "./alphabet";
 
 export const Footer = () => {
-  return <div>footer</div>;
+  const navigation = useNavigate();
+
+  const searchLink = (letter) => {
+    navigation({
+      pathname: "/cocktail",
+      search: `?f=${letter}`,
+    });
+  };
+
+  return (
+    <div className="footer">
+      <h2 className="footer__title">Browse By Name</h2>
+      <div className="footer__alfabet">
+        {alphabet.map((item) => (
+          <div key={item} className="footer__link">
+            <button onClick={() => searchLink(item)}>{item}</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
